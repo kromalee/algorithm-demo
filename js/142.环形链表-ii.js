34126257f0e431f198bd643e26a403dc820eeebc
kroma <kromalee2020@gmail.com>
2022-09-08 23:00:26 +0800

    feat 142

--- js/142.环形链表-ii.js
+++ js/142.环形链表-ii.js
@@ -18,7 +18,24 @@
  * @return {ListNode}
  */
 var detectCycle = function (head) {
-    
+    var slow = head
+    var fast = head
+    while (fast && fast.next) {
+        fast = fast.next.next
+        slow = slow.next;
+        if (slow === fast) {
+            break;
+        }
+    }
+    if (fast === null || fast.next === null) {
+        return null
+    }
+    slow = head
+    while (slow !== fast) {
+        slow = slow.next
+        fast = fast.next
+    }
+    return slow
 };
 // @lc code=end
 
