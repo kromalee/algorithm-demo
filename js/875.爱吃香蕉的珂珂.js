d0f0b049534e3d85e34fca9d9bacdd039d22e284
kroma <kromalee2020@gmail.com>
2022-10-08 23:39:38 +0800

    feat 875

--- js/875.爱吃香蕉的珂珂.js
+++ js/875.爱吃香蕉的珂珂.js
@@ -5,13 +5,40 @@
  */
 
 // @lc code=start
+
+var f = function (piles, x) {
+    var hrs = 0
+    for (var i = 0; i < piles.length; i++) {
+        hrs += Math.floor(piles[i] / x)
+        if (piles[i] % x > 0) {
+            hrs++
+        }
+    }
+    return hrs
+}
+
 /**
  * @param {number[]} piles
  * @param {number} h
  * @return {number}
  */
-var minEatingSpeed = function(piles, h) {
+var minEatingSpeed = function (piles, h) {
+    var left = 0;
+    var right = 1000000000 + 1
+    while (left < right) {
+        var mid = left + Math.floor((right - left) / 2)
+        if (f(piles, mid) === h) {
+            right = mid
+        }
+        else if (f(piles, mid) > h) {
+            left = mid + 1
+        }
+        else if (f(piles, mid) < h) {
+            right = mid
+        }
 
+    }
+    return left
 };
 // @lc code=end
-
+console.log(minEatingSpeed([3, 6, 7, 11], 8))
