01eb9e8f3303f9df4b479bbf4037db8bbdb37e1e
kroma <kromalee2020@gmail.com>
2022-10-25 00:09:29 +0800

    plan for 1.2 1.3

---
+++ js/1081.不同字符的最小子序列.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode.cn id=1081 lang=javascript
+ *
+ * [1081] 不同字符的最小子序列
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var smallestSubsequence = function(s) {
+
+};
+// @lc code=end
+
---
+++ js/710.黑名单中的随机数.js
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode.cn id=710 lang=javascript
+ *
+ * [710] 黑名单中的随机数
+ */
+
+// @lc code=start
+/**
+ * @param {number} n
+ * @param {number[]} blacklist
+ */
+var Solution = function(n, blacklist) {
+
+};
+
+/**
+ * @return {number}
+ */
+Solution.prototype.pick = function() {
+
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * var obj = new Solution(n, blacklist)
+ * var param_1 = obj.pick()
+ */
+// @lc code=end
+
