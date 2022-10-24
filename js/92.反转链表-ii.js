0c8a2aa46429dbbc7761ebb7b49551bce6a37598
kroma <kromalee2020@gmail.com>
2022-10-24 22:53:35 +0800

    feat 92

--- js/92.反转链表-ii.js
+++ js/92.反转链表-ii.js
@@ -5,6 +5,22 @@
  */
 
 // @lc code=start
+
+// 后面不变的节点
+var successor = null
+var reverseN = function (head, n) {
+    if (n == 1) {
+        successor = head.next
+        return head
+    }
+    var last = reverseN(head.next, n - 1)
+    head.next.next = head
+    head.next = successor
+    return last
+
+}
+
+
 /**
  * Definition for singly-linked list.
  * function ListNode(val, next) {
@@ -12,14 +28,22 @@
  *     this.next = (next===undefined ? null : next)
  * }
  */
+
+
+
+
 /**
  * @param {ListNode} head
  * @param {number} left
  * @param {number} right
  * @return {ListNode}
  */
-var reverseBetween = function(head, left, right) {
-
+var reverseBetween = function (head, left, right) {
+    if (left === 1) {
+        return reverseN(head, right)
+    }
+    head.next = reverseBetween(head.next, left - 1, right - 1)
+    return head
 };
 // @lc code=end
 
