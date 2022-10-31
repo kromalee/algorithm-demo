0df3b9c13648f6a9f4254d28d209dcdd6c7281db
kroma <kromalee2020@gmail.com>
2022-10-31 22:54:02 +0800

    feat 20

--- js/20.有效的括号.js
+++ js/20.有效的括号.js
@@ -4,13 +4,49 @@
  * [20] 有效的括号
  */
 
+
+
 // @lc code=start
+
+/**
+ * 
+ * @param {*} str 
+ * @returns 
+ */
+function leftOf(str) {
+    if (str === ')') {
+        return '('
+    }
+    if (str === '}') {
+        return '{'
+    }
+    if (str === ']') {
+        return '['
+    }
+}
+
 /**
  * @param {string} s
  * @return {boolean}
  */
-var isValid = function(s) {
-
+var isValid = function (s) {
+    var arr = s.split('')
+    var stack = []
+    for (let index = 0; index < arr.length; index++) {
+        const element = arr[index];
+        if (['{', '[', '('].indexOf(element) !== -1) {
+            stack.push(element)
+        }
+        else if (stack.length !== 0 && stack[stack.length - 1] === leftOf(element)) {
+            stack.pop()
+        }
+        else {
+            return false
+        }
+    }
+    return stack.length === 0
 };
 // @lc code=end
+
+console.log(isValid('()'))
 
