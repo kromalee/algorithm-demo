6e91952c23cc4c41950a87aea9539e8c6c12068f
kroma <kromalee2020@gmail.com>
2022-11-07 23:18:33 +0800

    feat  921 1451

--- js/1541.平衡括号字符串的最少插入次数.js
+++ js/1541.平衡括号字符串的最少插入次数.js
@@ -9,8 +9,27 @@
  * @param {string} s
  * @return {number}
  */
-var minInsertions = function(s) {
-
+var minInsertions = function (s) {
+    var insert = 0; //插入的括号
+    var need = 0    //需要的括号
+    for (let index = 0; index < s.length; index++) {
+        const element = s[index];
+        if (element === '(') {
+            need += 2
+            if (need % 2 === 1) {
+                insert++
+                need--
+            }
+        }
+        else {
+            need--
+            if (need === -1) {
+                insert++
+                need = 1
+            }
+        }
+    }
+    return insert + need
 };
 // @lc code=end
 
--- js/921.使括号有效的最少添加.js
+++ js/921.使括号有效的最少添加.js
@@ -9,8 +9,24 @@
  * @param {string} s
  * @return {number}
  */
-var minAddToMakeValid = function(s) {
+var minAddToMakeValid = function (s) {
+    var insertLeft = 0
+    var needRight = 0
+    for (let index = 0; index < s.length; index++) {
+        const element = s[index];
+        if (element === '(') {
+            needRight++
+        }
+        else {
+            needRight--
+            if (needRight === -1) {
+                needRight = 0;
+                insertLeft++
+            }
+        }
 
+    }
+    return insertLeft + needRight
 };
 // @lc code=end
 
