326703103a3ce339f05d026e06fb3feacc807a4d
kroma <kromalee2020@gmail.com>
2022-11-12 14:48:49 +0800

    feat 739

--- js/739.每日温度.js
+++ js/739.每日温度.js
@@ -9,8 +9,19 @@
  * @param {number[]} temperatures
  * @return {number[]}
  */
-var dailyTemperatures = function(temperatures) {
-
+var dailyTemperatures = function (temperatures) {
+    var result = []
+    var stack = []
+    for (let index = temperatures.length - 1; index >= 0; index--) {
+        const element = temperatures[index];
+        while (stack.length !== 0 && temperatures[stack[stack.length - 1]] <= element) {
+            stack.pop()
+        }
+        result[index] = stack.length === 0 ? 0 : stack[stack.length - 1] - index
+        stack.push(index)
+    }
+    return result
 };
 // @lc code=end
 
+console.log(dailyTemperatures([73, 74, 75, 71, 69, 72, 76, 73]))
