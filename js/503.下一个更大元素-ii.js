e136868ae4f193fc0a5c94c2deb45d1373874369
kroma <kromalee2020@gmail.com>
2022-11-15 22:28:56 +0800

    feat 503

--- js/503.下一个更大元素-ii.js
+++ js/503.下一个更大元素-ii.js
@@ -9,8 +9,17 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var nextGreaterElements = function(nums) {
-
+var nextGreaterElements = function (nums) {
+    var result = Array(nums.length)
+    var stack = []
+    for (var idx = 2 * (nums.length) - 1; idx >= 0; idx--) {
+        while (stack.length !== 0 && stack[stack.length - 1] <= nums[idx % nums.length]) {
+            stack.pop()
+        }
+        result[idx % nums.length] = stack.length === 0 ? -1 : stack[stack.length - 1]
+        stack.push(nums[idx % nums.length])
+    }
+    return result
 };
 // @lc code=end
 
