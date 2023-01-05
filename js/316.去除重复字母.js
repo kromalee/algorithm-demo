0843ef8bd3c6fb3b5ef8564ba2a63b233706644c
kroma <kromalee2020@gmail.com>
2023-01-05 23:04:44 +0800

    feat 316

--- js/316.去除重复字母.js
+++ js/316.去除重复字母.js
@@ -22,27 +22,31 @@ var removeDuplicateLetters = function (s) {
         countStk[element.charCodeAt(0)]++
     }
 
-
     for (let index = 0; index < sArr.length; index++) {
         const element = sArr[index];
+        countStk[element.charCodeAt(0)]--
         if (inStk[element.charCodeAt(0)]) {
             continue;
         }
         else {
             // 放之前先检查是否最小
-            if (stk.length > 0 && element.charCodeAt(0) < stk[stk.length - 1].charCodeAt(0)) {
-                if (countStk[element.charCodeAt(0)]) {
-                    stk.pop()
-                    inStk[element.charCodeAt(0)] = false
+            while (stk.length > 0 && element.charCodeAt(0) < stk[stk.length - 1].charCodeAt(0)) {
+                if (countStk[stk[stk.length - 1].charCodeAt(0)]) {
+                    inStk[(stk.pop()).charCodeAt(0)] = false
+                }
+                else {
+                    break;
                 }
             }
             stk.push(element)
             inStk[element.charCodeAt(0)] = true
         }
-        countStk[element.charCodeAt(0)]--
+       
     }
-    return stk.reverse().join('')
+    return stk.join('')
 };
 // @lc code=end
 
-console.log(removeDuplicateLetters("bcabc"))
+// console.log(removeDuplicateLetters("bcabc"))
+// console.log(removeDuplicateLetters("cbacdcbc"))
+console.log(removeDuplicateLetters("bbcaac"))
