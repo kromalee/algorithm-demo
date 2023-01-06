52920b6edce9a97a9288055c676a8571d1d8b852
kroma <kromalee2020@gmail.com>
2023-01-06 23:23:17 +0800

    feat 1081

--- js/1081.不同字符的最小子序列.js
+++ js/1081.不同字符的最小子序列.js
@@ -9,8 +9,40 @@
  * @param {string} s
  * @return {string}
  */
-var smallestSubsequence = function(s) {
+var smallestSubsequence = function (s) {
+    var stk = []
+    var inStk = Array(255).fill(false)
+    var countStk = Array(255).fill(0)
+
+    var sArr = s.split('')
+    for (let index = 0; index < sArr.length; index++) {
+        const element = sArr[index];
+        countStk[element.charCodeAt(0)]++
+    }
+
+    for (let index = 0; index < sArr.length; index++) {
+        const element = sArr[index];
+        var elementChartCode = element.charCodeAt(0)
+        countStk[elementChartCode]--
+
+        if (inStk[elementChartCode]) {
+            continue;
+        }
+        while (stk.length > 0 && stk[stk.length - 1].charCodeAt(0) > elementChartCode) {
+            if (countStk[stk[stk.length - 1].charCodeAt(0)]) {
+                inStk[stk.pop().charCodeAt(0)] = false
+            }
+            else {
+                break;
+            }
+        }
+        stk.push(element)
+        inStk[elementChartCode] = true
+    }
+    return stk.join('')
 
 };
 // @lc code=end
+
+console.log(smallestSubsequence('bcabc'))
 
