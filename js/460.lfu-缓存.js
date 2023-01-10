9413623f384dccb436dbf287c94fd0061e19bce0
kroma <kromalee2020@gmail.com>
2023-01-10 23:20:48 +0800

    feat 460

--- js/460.lfu-缓存.js
+++ js/460.lfu-缓存.js
@@ -17,28 +17,52 @@ var LFUCache = function (capacity) {
     this.cap = capacity
 };
 
-LRUCache.prototype.increaseFreq = function (key) {
+LFUCache.prototype.increaseFreq = function (key) {
     // 修改key to freq
     var originalFreq = this.keyToFreq.get(key) || 0
-    var newFreq = originalFreq++
+    var newFreq = originalFreq + 1
     this.keyToFreq.set(key, newFreq)
     // 修改freq to keys
 
     // 修改原始freq to keys
     var originalKeys = this.freqToKeys.get(originalFreq)
-    if (originalFreq) {
-        originalFreq.delete(key)
+    if (originalKeys) {
+        originalKeys.delete(key)
     }
 
     // 修改新的freq to keys
-    var newFreqKeys = this.freqToKeys.get(newFreq)
-    newFreqKeys.add(key)
-    if (originalFreq === this.minFreq) {
-        this.minFreq++
+    if (this.freqToKeys.has(newFreq)) {
+        var newFreqKeys = this.freqToKeys.get(newFreq).add(key)
+    }
+    else {
+        var set = new Set()
+        set.add(key)
+        this.freqToKeys.set(newFreq, set)
+    }
+
+    if (!originalKeys) {
+        if (originalFreq === this.minFreq) {
+            this.minFreq++
+        }
+    }
+    if (originalKeys && originalKeys.size === 0) {
+        this.freqToKeys.delete(originalFreq)
+        if (originalFreq === this.minFreq) {
+            this.minFreq++
+        }
     }
 }
-LRUCache.prototype.removeMinFreqKey = function () {
-
+LFUCache.prototype.removeMinFreqKey = function () {
+    var keys = this.freqToKeys.get(this.minFreq)
+    if (keys) {
+        var delKey = keys.keys().next().value
+        keys.delete(delKey)
+        if (keys.size === 0) {
+            this.freqToKeys.delete(this.minFreq)
+        }
+        this.keyToVal.delete(delKey)
+        this.keyToFreq.delete(delKey)
+    }
 }
 
 /** 
@@ -46,7 +70,7 @@ LRUCache.prototype.removeMinFreqKey = function () {
  * @return {number}
  */
 LFUCache.prototype.get = function (key) {
-    if (!this.keyToVal.get(key)) {
+    if (!this.keyToVal.has(key)) {
         return -1
     }
     this.increaseFreq(key)
@@ -59,11 +83,34 @@ LFUCache.prototype.get = function (key) {
  * @return {void}
  */
 LFUCache.prototype.put = function (key, value) {
-    if (this.keyToVal.size === this.cap) {
-        this.removeMinFreqKey()
+    // 已有键,直接更新
+    if (this.keyToVal.has(key)) {
+        this.keyToVal.set(key, value)
+        this.increaseFreq(key)
     }
-    this.keyToVal.set(key, value)
-    this.increaseFreq(key)
+    else {
+        // 容量为0直接跳过
+        if (this.cap === 0) {
+            return
+        }
+        // 是一个新键
+        if (this.keyToVal.size === this.cap) {
+            this.removeMinFreqKey()
+        }
+        this.keyToVal.set(key, value)
+        this.keyToFreq.set(key, 1)
+        if (!this.freqToKeys.get(1)) {
+            this.freqToKeys.set(1, new Set())
+            this.freqToKeys.get(1).add(key)
+        }
+        else {
+            this.freqToKeys.get(1).add(key)
+        }
+        this.minFreq = 1
+    }
+
+
+
 };
 
 /**
@@ -73,4 +120,8 @@ LFUCache.prototype.put = function (key, value) {
  * obj.put(key,value)
  */
 // @lc code=end
+// var lfu = new LFUCache(2)
+var lfu = new LFUCache(0);
+console.log(lfu.put(0, 0))
+console.log(lfu.get(0))
 
