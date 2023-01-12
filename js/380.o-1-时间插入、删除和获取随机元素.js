71b096b1a64215caa131fae541b03529f09909cd
kroma <kromalee2020@gmail.com>
2023-01-12 22:23:52 +0800

    feat 380

--- js/380.o-1-时间插入、删除和获取随机元素.js
+++ js/380.o-1-时间插入、删除和获取随机元素.js
@@ -6,31 +6,45 @@
 
 // @lc code=start
 
-var RandomizedSet = function() {
-
+var RandomizedSet = function () {
+    this.nums = []
+    this.valToIndex = new Map()
 };
 
 /** 
  * @param {number} val
  * @return {boolean}
  */
-RandomizedSet.prototype.insert = function(val) {
-
+RandomizedSet.prototype.insert = function (val) {
+    if (!this.valToIndex.has(val)) {
+        this.nums.push(val)
+        this.valToIndex.set(val, this.nums.length - 1)
+        return true
+    }
+    return false
 };
 
 /** 
  * @param {number} val
  * @return {boolean}
  */
-RandomizedSet.prototype.remove = function(val) {
-
+RandomizedSet.prototype.remove = function (val) {
+    if (this.valToIndex.has(val)) {
+        var swap = this.nums[this.nums.length - 1]
+        this.nums[this.nums.length - 1] = this.nums[this.valToIndex.get(val)]
+        this.nums[this.valToIndex.get(val)] = swap
+        this.valToIndex.set(swap, this.valToIndex.get(val))
+        this.valToIndex.delete(this.nums.pop())
+        return true
+    }
+    return false
 };
 
 /**
  * @return {number}
  */
-RandomizedSet.prototype.getRandom = function() {
-
+RandomizedSet.prototype.getRandom = function () {
+    return this.nums[Math.floor(Math.random() * this.nums.length)]
 };
 
 /**
@@ -42,3 +56,11 @@ RandomizedSet.prototype.getRandom = function() {
  */
 // @lc code=end
 
+var randomizedSet = new RandomizedSet();
+randomizedSet.insert(1); // 向集合中插入 1 。返回 true 表示 1 被成功地插入。
+randomizedSet.remove(2); // 返回 false ，表示集合中不存在 2 。
+randomizedSet.insert(2); // 向集合中插入 2 。返回 true 。集合现在包含 [1,2] 。
+randomizedSet.getRandom(); // getRandom 应随机返回 1 或 2 。
+randomizedSet.remove(1); // 从集合中移除 1 ，返回 true 。集合现在包含 [2] 。
+randomizedSet.insert(2); // 2 已在集合中，所以返回 false 。
+randomizedSet.getRandom(); // 由于 2 是集合中唯一的数字，getRandom 总是返回 2 。
