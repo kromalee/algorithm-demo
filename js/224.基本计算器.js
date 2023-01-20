8c2283a6018cd8f8bfa7519d5abcf3b56f13e623
kroma <kromalee2020@gmail.com>
2023-01-21 00:05:21 +0800

    feat 224

--- js/224.基本计算器.js
+++ js/224.基本计算器.js
@@ -17,48 +17,48 @@ function isDigital(element) {
  * @return {number}
  */
 var calculate = function (s) {
-    if (typeof s === 'string') {
-        var sArr = s.split('')
-    }
-    else {
-        var sArr = s
-    }
-    var stk = []
-    var num = 0
-    var sign = '+'
-    while (sArr.length > 0) {
-        var element = sArr.shift()
-        if (element && isDigital(element)) {
-            num = num * 10 + (element.charCodeAt(0) - '0'.charCodeAt(0))
-        }
-        if (element === '(') {
-            num = calculate(sArr)
-        }
-        if ((element && !isDigital(element) && element !== ' ') || (sArr.length === 0)) {
-            switch (sign) {
-                case '+':
-                    stk.push(num)
-                    break;
-                case '-':
-                    stk.push(-num)
-                    break;
-                case '*':
-                    stk.push(stk[stk.length - 1] * num)
-                    break;
-                case '/':
-                    stk.push(stk[stk.length - 1] / num)
-                    break;
-                default:
-                    break;
+    var i = 0
+    function helper() {
+        var stk = []
+        var num = 0
+        var sign = '+'
+        while (i < s.length) {
+            var element = s[i]
+            i++
+
+            if (element && isDigital(element)) {
+                num = num * 10 + (element.charCodeAt(0) - '0'.charCodeAt(0))
             }
-            sign = element
-            num = 0
+            if (element === '(') {
+                num = helper()
+            }
+            if ((element && !isDigital(element) && element !== ' ') || (i === s.length)) {
+                switch (sign) {
+                    case '+':
+                        stk.push(num)
+                        break;
+                    case '-':
+                        stk.push(-num)
+                        break;
+                    case '*':
+                        stk.push(stk[stk.length - 1] * num)
+                        break;
+                    case '/':
+                        stk.push(stk[stk.length - 1] / num)
+                        break;
+                    default:
+                        break;
+                }
+                sign = element
+                num = 0
+            }
+            if (element === ')') {
+                break;
+            }
         }
-        if (element === ')') {
-            return stk.reduce((acc, cur) => { return acc + cur }, 0)
-        }
+        return stk.reduce((acc, cur) => { return acc + cur }, 0)
     }
-    return stk.reduce((acc, cur) => { return acc + cur }, 0)
+    return helper()
 };
 // @lc code=end
 // console.log(calculate("1+(1+1)"))
@@ -66,4 +66,4 @@ var calculate = function (s) {
 // console.log(calculate("(1+(4+5+2)-3)+(6+8)"))
 console.log(calculate(" 2-1 + 2 "))
 
-// 超时问题  可能时两次计算 stk
+// 超时问题  不能使用数组去模拟字符串
